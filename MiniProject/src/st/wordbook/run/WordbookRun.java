07ce2a2289db9b6818384c19f5e6a8d7e7e228c6
dan9708 <thdgus0829@gmail.com>
2020-12-10 21:08:44 +0900

    수정

--- MiniProject/src/st/wordbook/run/WordbookRun.java
+++ MiniProject/src/st/wordbook/run/WordbookRun.java
@@ -1,7 +1,7 @@
 package st.wordbook.run;
 
 import st.wordbook.view.EngTestFrame;
-import st.wordbook.view.WordbookFrame;
+import st.wordbook.view.NavFrame;
 
 /**
  * 프로그램 윈도우창 생성자이용 --> 호출 및 실행
@@ -11,7 +11,8 @@ public class WordbookRun {
 	public static void main(String[] args) {
 		// 윈도우창 크기는 UI짜시면서 필요에 따라서 조절해주세요.
 //	    new WordbookFrame().setVisible(true);
-	    new EngTestFrame().setVisible(true);
+//	    new EngTestFrame().setVisible(true);
+		new NavFrame().setVisible(true);
 	}
 
 }
